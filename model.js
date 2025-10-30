d8724e67e38518328cb233e5b68b184427f83c67
Piet van Blokland <pjvanblokland@gmail.com>
2025-10-30 13:51:18 +0100

    Wachtwoord weghalen uit code

--- model.js
+++ model.js
@@ -52,7 +52,8 @@ function testcode() {
 }
 
 function startheroku() {
-    url = heroku + "?dataset=" + model.numcode() + "&language = " + taal;
+    // Direct naar de lokale graph.html file zonder language parameter (gebruikt localStorage)
+    url = "heroku/graph.html?dataset=" + model.numcode();
     window.open(url, "_blank");
 }
 
@@ -66,7 +67,7 @@ function starten(toonheroku) {
         }
     } else {
         if (testcode()) {
-            //naamurl = heroku + 'exists?nummer=' + model.numcode()+'&wachtwoord=""&code=0';
+            //naamurl = heroku + 'exists?nummer=' + model.numcode()+'&code=0';
             naamurl = heroku + 'bestaat?nummer=' + model.numcode();
             $.getJSON(naamurl, function (data) {
                 if (data.exists) {
@@ -96,9 +97,9 @@ function sluitdialogs() {
 
 
 
-function bestaat_nummer_paswoord_code(nummer, wachtwoord, code, dialogs) {
+function bestaat_nummer_code(nummer, code, dialogs) {
     var url;
-    var naamurl = heroku + 'exists/?number=' + nummer + '&wachtwoord="' + wachtwoord + '"&code=' + code;
+    var naamurl = heroku + 'exists/?number=' + nummer + '&code=' + code;
     console.log(naamurl);
 
     sluitdialogs();
@@ -122,88 +123,78 @@ function bestaat_nummer_paswoord_code(nummer, wachtwoord, code, dialogs) {
 
     } else {
         //  if ((code == schoon) || (code == nieuw) || (code == verwijder)) {
-        if (wachtwoord == '') {
-            alert(wachtwoord_vereist[taal]);
-            return false;
 
-        }
-        //  }
 
         //"testen op 123446"
         $.getJSON(naamurl, function (data) {
             console.log('antwoord naamurl ', naamurl, 'data ', data);
             switch (code) {
-                case nieuw: //wachtwoord niet vereist
+                case nieuw:
                     if (data.exists) { //nieuw
                         alert(vulin(dataset_created, nummer));
                     } else {
                         alert(vulin(dataset_betaat_al[taal], nummer));
                     }
                     break;
-                case schoon: //clear  wachtwword vereist
+                case schoon:
                     if (data.exists) {
-                        if (data.wachtwoord!="") {
-                            alert(vulin(made_empty[taal], 123446));
-                        } else {
-                            alert(invalid_password[taal]);
-                        }
+
+                        alert(vulin(made_empty[taal], 123446));
+
                     } else {
                         alert(vulin(Dataset_bestaat_niet[taal], nummer));
                     }
                     break;
                 case verwijder:
                     if (data.exists) {
-                        if (data.wachtwoord!="") {
-                            alert(dataset_verwijderd[taal]);
+                        if (confirm(vulin(ben_zeker_verwijderen[taal], nummer))) {
                         } else {
-                            alert(invalid_password[taal]);
+                            alert(vulin(data_set_bestaat_niet[taal], nummer));
                         }
-                    } else {
-                        alert(vulin(data_set_bestaat_niet[taal], nummer));
+                        break;
                     }
-                    break;
+            };
+        })
+    }
+
+    function Model() {
+
+        var self = this, i;
+        this.url = ko.observable('help');
+        this.M_TALEN = TALEN;
+        this.selectedTaal = ko.observable(-1);
+        this.selectedTaal.subscribe(function () {
+            taal = self.selectedTaal();
+            if (taal >= 0) {
+                localStorage.setItem('language', taal);
+                self.url("../help/" + self.M_TALEN[taal].code + "/index.html?fcount.htm");
+                zettaal(taal);
+
+            }
+
+        });
+
+        this.aantalkeer = ko.observable(0);
+        this.firstPage = ko.observable(true);
+        this.numcode = ko.observable(123446);
+
+        this.nieuwcode = ko.observable("123456");
+
+        this.nog_insturen = ko.observable(true);
+        this.nog_insturen.subscribe(function () {
+            if (!(self.nog_insturen())) {
+                self.text_OK(view_dataset[taal]);
             }
         });
+
+        this.text_OK = ko.observable(alg_OK[0]); // Start met Engels, wordt later bijgewerkt
+        this.contact_internet = ko.observable(-1);
+        //-1 vraag om te beginnen //kan alleen bij bestaande code
+        //0 new Is deze dataset echt nieuw  en geef niet leeg wachtwoord
+        //1  //clear bestaat dataset en vraag om wachtwoord en bevestiging
+        //2  //delete dataset dataset en vraag om wachtwoord en bevestiging
+
+
+
+
     }
-}
-function Model() {
-
-    var self = this, i;
-    this.url = ko.observable('help');
-    this.M_TALEN = TALEN;
-    this.selectedTaal = ko.observable(-1);
-     this.selectedTaal.subscribe(function () {
-        taal = self.selectedTaal();
-        if (taal >= 0) {
-            localStorage.setItem('language', taal);
-            self.url("../help/" + self.M_TALEN[taal].code + "/index.html?fcount.htm");
-            zettaal(taal);
-          
-        }
-
-    });
-   
-    this.aantalkeer = ko.observable(0);
-    this.firstPage = ko.observable(true);
-    this.numcode = ko.observable(123446);
-    this.password = ko.observable('');
-    this.nieuwcode=ko.observable("123456");
-
-    this.nog_insturen = ko.observable(true);
-    this.nog_insturen.subscribe(function () {
-        if (!(self.nog_insturen())) {
-            self.text_OK(view_dataset[taal]);
-        }
-    });
-
-    this.text_OK = ko.observable(alg_OK[0]); // Start met Engels, wordt later bijgewerkt
-    this.contact_internet = ko.observable(-1);
-    //-1 vraag om te beginnen //kan alleen bij bestaande code
-    //0 new Is deze dataset echt nieuw  en geef niet leeg wachtwoord
-    //1  //clear bestaat dataset en vraag om wachtwoord en bevestiging
-    //2  //delete dataset dataset en vraag om wachtwoord en bevestiging
-
-
-
-
-}
